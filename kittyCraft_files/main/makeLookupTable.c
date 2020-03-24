8a9f7d73938ff14ed56152d96518ec48deae4c8d
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2020-03-24 15:46:16 +0100

    Removed not so useful dump of addresses.

--- kittyCraft_files/main/makeLookupTable.c
+++ kittyCraft_files/main/makeLookupTable.c
@@ -72,7 +72,6 @@ char * _kittycraft_makeLookupTable(struct kittyCompactIFace *Self)
 
 	for (cmd=AMOSPro_CRAFT;cmd -> fn;cmd++)
 	{
-		Printf("%08lx\n",((char *(**) KITTENS_CMD_ARGS) (ptr + cmd -> token)));
 		*((char *(**) KITTENS_CMD_ARGS) (ptr + cmd -> token)) = cmd -> fn;
 	}
 
